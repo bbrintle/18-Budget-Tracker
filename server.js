83608827dbe3451a5a307441aa2c8735a97c5e5c
bbrintle <bbrintle@gmail.com>
2020-12-14 11:12:33 -0800

    comments added to server.js

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+//process.env.PORT gives the port for Heroku
 const PORT = process.env.PORT || 3000;
 
+//Set up express
 const app = express();
 
 app.use(logger("dev"));
@@ -15,6 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+//Below info provided by MongoDB Atlas and Heroku to connect to a mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
@@ -23,6 +26,7 @@ mongoose.connect(
   useFindAndModify: false
 });
 
+//Require the API routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
